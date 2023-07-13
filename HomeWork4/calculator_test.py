7c6f7c91797d2e4be53c4783cca9fc9756f91737
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-13 21:08:47 +0300

    save

--- HomeWork4/calculator_test.py
+++ HomeWork4/calculator_test.py
@@ -2,32 +2,46 @@ from calculator import Calculator
 
 calculator = Calculator()
 
-res = calculator.sum(4, 5)
-assert (res == 9)
+def test_sum_positive_nums():
+    calculator = Calculator()
+    res = calculator.sum(4,5)
+    assert res == 9
+def test_sum_negative_nums():
+    calculator = Calculator()
+    res = calculator.sum(-4,-5)
+    assert res == -9
+def test_sum_positive_and_negative_nums():
+    calculator = Calculator()
+    res = calculator.sum(-4,5)
+    assert res == 1
 
-res = calculator.sum (-6, -10)
-assert res == -16
 
-res = calculator.sum (-5, 10)
-assert res == 5
+#res = calculator.sum(4, 5)
+#assert (res == 9)
 
-res = calculator.sum (5.6, 4.3)
-res = round(res, 1)
-assert res == 9.9
+#res = calculator.sum (-6, -10)
+#assert res == -16
 
-res = calculator.sum (10, 0)
-assert res == 10
+#res = calculator.sum (-5, 10)
+#assert res == 5
 
-res =calculator.div(10, 2)
-assert res == 5
+#res = calculator.sum (5.6, 4.3)
+#res = round(res, 1)
+#assert res == 9.9
 
-numbers = []
-res = calculator.avg(numbers)
-assert res == 0
+#res = calculator.sum (10, 0)
+#assert res == 10
 
-numbers = [1,2,3]
-res = calculator.avg(numbers)
-assert res == 2
+#res =calculator.div(10, 2)
+#assert res == 5
 
-res = calculator.div(5, 0)
-assert res == None
+#numbers = []
+#res = calculator.avg(numbers)
+#assert res == 0
+
+#numbers = [1,2,3]
+#res = calculator.avg(numbers)
+#assert res == 2
+
+#res = calculator.div(5, 0)
+#assert res == None
