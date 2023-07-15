9930d8185afa2a197f308f2556e4480799b6228e
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 07:54:56 +0300

    changes

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -2,6 +2,7 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
+## Убираем пробелы перед словом
 def test_trim_positive():
     string_utils = StringUtils()
     res = string_utils.trim('   Anton')
@@ -10,42 +11,93 @@ def test_trim_negative():
     string_utils = StringUtils()
     res = string_utils.trim('   Anton')
     assert res == '  Anton'
-    
+
+## Выводим слово с заглавной  
 def test_capitilize():
     string_utils = StringUtils()
     res = string_utils.capitilize('anton')
     assert res == 'Anton'
+def test_capitilize_negative():
+    string_utils = StringUtils()
+    res = string_utils.capitilize('anton')
+    assert res == 'anton'
 
+## Превращаем значения в список
 def test_to_list():
     string_utils = StringUtils()
     res = string_utils.to_list('Ark')
     assert res == ['Ark']
+def test_to_list_negative():
+    string_utils = StringUtils()
+    res = string_utils.to_list('Ark')
+    assert res == 'Ark'
 
+## Поиск буквы в слове
 def test_contains():
     string_utils = StringUtils()
     res = string_utils.contains("Anton", "t")
     assert res == True
+def test_contains_negative():
+    string_utils = StringUtils()
+    res = string_utils.contains("Anton", "s")
+    assert res == True
+def test_contains_negative_two():
+    string_utils = StringUtils()
+    res = string_utils.contains("Anton", "o")
+    assert res == False
 
+## Удаление символов
 def test_delete_symbol():
     string_utils = StringUtils()
     res = string_utils.delete_symbol("Anton", "A")
     assert res == "nton"
+def test_delete_symbol_negative():
+    string_utils = StringUtils()
+    res = string_utils.delete_symbol("Anton", "A")
+    assert res == "Aton"
+def test_delete_symbol_negative_two():
+    string_utils = StringUtils()
+    res = string_utils.delete_symbol("Anton", "A")
+    assert res == "Anton"
 
+
+## Проверка буквы начала слова
 def test_starts_with():
     string_utils = StringUtils()
     res = string_utils.starts_with("Anton", "A")
     assert res == True
+def test_starts_with_negative():
+    string_utils = StringUtils()
+    res = string_utils.starts_with("Anton", "n")
+    assert res == False
 
+
+
+## Проверка буквы окончания слова
 def test_end_with():
     string_utils = StringUtils()
     res = string_utils.end_with("Anton", "n")
     assert res == True
+def test_end_with():
+    string_utils = StringUtils()
+    res = string_utils.end_with("Anton", "A")
+    assert res == False
 
+## Проверка пустой строки
 def test_is_empty():
     string_utils = StringUtils()
     res = string_utils.is_empty("")
     assert res == True
+def test_is_empty_negative():
+    string_utils = StringUtils()
+    res = string_utils.is_empty(123)
+    assert res == False
+def test_is_empty_negative_two():
+    string_utils = StringUtils()
+    res = string_utils.is_empty("Антон")
+    assert res == False
 
+## Перевод списка в строку
 def test_list_to_string():
     string_utils = StringUtils()
     res = string_utils.list_to_string(["A","n","t","o","n"])
