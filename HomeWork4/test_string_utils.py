c06c06da0363df5a91e09a83643e4f555f1dd135
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 07:01:37 +0300

    one

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -2,3 +2,7 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
+def test_string_with_space():
+    string_utils = StringUtils()
+    res = string_utils.trim('   Anton')
+    assert res == 'Anton'
