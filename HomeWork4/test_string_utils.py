935ca36b2186216de122a928cf97a65b9cbc9d41
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 08:22:01 +0300

    TEST

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -4,7 +4,10 @@ from string_utils import StringUtils
 string_utils = StringUtils()
 
 ## Убираем пробелы перед словом
-@pytest.mark.parametrize('str', 'result', [('  Anton', 'Anton'), ('  4 april 2023', '4 april 2023')])
+@pytest.mark.parametrize('str', 'result',[
+("  Anton", "Anton"), 
+("  4 april 2023", "4 april 2023")
+])
 def test_trim_positive(str, result):
     string_utils = StringUtils()
     res = string_utils.trim(str)
