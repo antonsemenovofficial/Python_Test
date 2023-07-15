88192ce8c144607a99f1182f076ae9db297bcbf2
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 07:05:24 +0300

    test1

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -6,3 +6,8 @@ def test_string_with_space():
     string_utils = StringUtils()
     res = string_utils.trim('   Anton')
     assert res == 'Anton'
+    
+def test_capitilize():
+    string_utils = StringUtils()
+    res = string_utils.capitilize('anton')
+    assert res == 'Anton'
