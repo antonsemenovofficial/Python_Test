d6ca801ca31ebc7991b32f1fae962e843418a5e0
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 07:39:17 +0300

    add test

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -2,7 +2,7 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
-def test_string_with_space():
+def test_trim():
     string_utils = StringUtils()
     res = string_utils.trim('   Anton')
     assert res == 'Anton'
@@ -11,3 +11,38 @@ def test_capitilize():
     string_utils = StringUtils()
     res = string_utils.capitilize('anton')
     assert res == 'anton'
+
+def test_to_list():
+    string_utils = StringUtils()
+    res = string_utils.to_list("A","n","t","o","n")
+    assert res == ["A","n","t","o","n"]
+
+def test_contains():
+    string_utils = StringUtils()
+    res = string_utils.contains("Anton", "t")
+    assert res == True
+
+def test_delete_symbol():
+    string_utils = StringUtils()
+    res = string_utils.delete_symbol("Anton", "A")
+    assert res == "nton"
+
+def test_starts_with():
+    string_utils = StringUtils()
+    res = string_utils.starts_with("Anton", "A")
+    assert res == True
+
+def test_end_with():
+    string_utils = StringUtils()
+    res = string_utils.end_with("Anton", "n")
+    assert res == True
+
+def test_is_empty():
+    string_utils = StringUtils()
+    res = string_utils.is_empty("")
+    assert res == True
+
+def test_list_to_string():
+    string_utils = StringUtils()
+    res = string_utils.list_to_string(["A","n","t","o","n"])
+    assert res == ("A","n","t","o","n")
