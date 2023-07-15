0540ed795b0783660d25ffbd35f8e4a37228ceb2
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-15 06:56:32 +0300

    test

--- HomeWork4/calculator_test.py
+++ HomeWork4/calculator_test.py
@@ -2,18 +2,18 @@ from calculator import Calculator
 
 calculator = Calculator()
 
-def test_sum_positive_nums():
-    calculator = Calculator()
-    res = calculator.sum(4,5)
-    assert res == 9
-def test_sum_negative_nums():
-    calculator = Calculator()
-    res = calculator.sum(-4,-5)
-    assert res == -9
-def test_sum_positive_and_negative_nums():
-    calculator = Calculator()
-    res = calculator.sum(-5,5)
-    assert res == 1
+#def test_sum_positive_nums():
+#    calculator = Calculator()
+#    res = calculator.sum(4,5)
+#    assert res == 9
+#def test_sum_negative_nums():
+ #   calculator = Calculator()
+ #   res = calculator.sum(-4,-5)
+ #   assert res == -9
+#def test_sum_positive_and_negative_nums():
+#    calculator = Calculator()
+#    res = calculator.sum(-5,5)
+#    assert res == 1
 
 
 #res = calculator.sum(4, 5)
