4a6be6dee5ac9cf12afb4ff4ff1596be8622663d
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-16 10:01:45 +0300

    HomeWork6

---
+++ HomeWork6/Lessons/elements_properties.py
@@ -0,0 +1,65 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver.get("https://yandex.ru")
+
+# txt = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").text
+# tag = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").tag_name
+# id = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").id
+# href = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").get_attribute("href")
+# ff = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").value_of_css_property("font-family")
+# color = driver.find_element(By.CSS_SELECTOR,"a[aria-label='Курс USD/RUB']").value_of_css_property("color")
+
+# print(txt)
+# print(tag)
+# print(id)
+# print(href)
+# print(ff)
+# print(color)
+
+# driver.get("http://www.uitestingplayground.com/visibility")
+
+# is_dispayed = driver.find_element(By.CSS_SELECTOR, "#transparentButton").is_displayed()
+# print(is_dispayed)
+# sleep(2)
+# driver.find_element(By.CSS_SELECTOR, "#hideButton").click()
+# sleep(2)
+# is_dispayed = driver.find_element(By.CSS_SELECTOR, "#transparentButton").is_displayed()
+# print(is_dispayed)
+# sleep(2)
+
+# driver.get("https://demoqa.com/radio-button")
+
+# is_enabled = driver.find_element(By.CSS_SELECTOR, "#yesRadio").is_enabled()
+# print(is_enabled)
+
+# driver.get("https://the-internet.herokuapp.com/checkboxes")
+
+# cb = driver.find_element(By.CSS_SELECTOR, "input[type=checkbox]")
+
+# is_selected = cb.is_selected()
+# print(is_selected)
+
+# cb.click()
+
+# is_selected = cb.is_selected()
+# print(is_selected)
+
+driver.get("https://the-internet.herokuapp.com/checkboxes")
+
+# div = driver.find_element(By.CSS_SELECTOR,"#page-footer")
+
+# a = div.find_element(By.CSS_SELECTOR, "a")
+
+# print(a.get_attribute("href"))
+
+divs = driver.find_elements(By.CSS_SELECTOR, "div")
+
+div = divs[6]
+css_class = div.get_attribute("class")
+print(css_class)
---
+++ HomeWork6/Lessons/implicity_wait.py
@@ -0,0 +1,17 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(20)
+driver.get("http://uitestingplayground.com/ajax")
+
+driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+
+content = driver.find_element(By.CSS_SELECTOR, "#content")
+txt = content.find_element(By.CSS_SELECTOR,"p.bg-success").text
+print(txt)
+
+
