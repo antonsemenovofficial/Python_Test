d14a4605144dd22ccd6ec003a21412fd4fecb6ca
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-16 08:35:57 +0300

    changes

--- HomeWork4/test_string_utils.py
+++ HomeWork4/test_string_utils.py
@@ -4,17 +4,18 @@ from string_utils import StringUtils
 string_utils = StringUtils()
 
 ## Убираем пробелы перед словом
-def test_trim_positive():
+def test_trim():
     string_utils = StringUtils()
     res = string_utils.trim('  Anton')
     assert res == 'Anton'
-def test_trim_positive_two():
+def test_trim():
     string_utils = StringUtils()
     res = string_utils.trim('  4 april 2023')
     assert res == '4 april 2023'
 
+## Негативные тест кейсы
 @pytest.mark.xfail(strict=True)
-def test_trim_negative():
+def test_trim_n1():
     string_utils = StringUtils()
     res = string_utils.trim('   Anton')
     assert res == '  Anton'
@@ -24,22 +25,24 @@ def test_capitilize():
     string_utils = StringUtils()
     res = string_utils.capitilize('anton')
     assert res == 'Anton'
+
+## Негативный тест кейс
 @pytest.mark.xfail(strict=True)
-def test_capitilize_negative():
+def test_capitilize_n1():
     string_utils = StringUtils()
-    res = string_utils.capitilize('anton')
-    assert res == 'anton'
+    res = string_utils.capitilize('Anton')
+    assert res == 'Anton'
+@pytest.mark.xfail(strict=True)
+def test_capitilize_n2():
+    string_utils = StringUtils()
+    res = string_utils.capitilize('')
+    assert res == 'Anton'
 
 ## Превращаем значения в список
 def test_to_list():
     string_utils = StringUtils()
     res = string_utils.to_list('Ark')
     assert res == ['Ark']
-@pytest.mark.xfail(strict=True)
-def test_to_list_negative():
-    string_utils = StringUtils()
-    res = string_utils.to_list('Ark')
-    assert res == 'Ark'
 def test_to_list_two():
     string_utils = StringUtils()
     res = string_utils.to_list("")
@@ -49,15 +52,24 @@ def test_to_list_three():
     res = string_utils.to_list(" ")
     assert res == []
 
+## Негативные проверки
+@pytest.mark.xfail(strict=True)
+def test_to_list_n1():
+    string_utils = StringUtils()
+    res = string_utils.to_list('Ark')
+    assert res == 'Ark'
+
 ## Поиск буквы в слове
 def test_contains():
     string_utils = StringUtils()
     res = string_utils.contains("Anton", "t")
     assert res == True
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
 def test_contains_negative():
     string_utils = StringUtils()
-    res = string_utils.contains("Anton", "s")
+    res = string_utils.contains("Anton", 1)
     assert res == True
 @pytest.mark.xfail(strict=True)
 def test_contains_negative_two():
@@ -70,15 +82,17 @@ def test_delete_symbol():
     string_utils = StringUtils()
     res = string_utils.delete_symbol("Anton", "A")
     assert res == "nton"
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
-def test_delete_symbol_negative():
+def test_delete_symbol_n1():
     string_utils = StringUtils()
-    res = string_utils.delete_symbol("Anton", "A")
+    res = string_utils.delete_symbol("Anton", "")
     assert res == "Aton"
 @pytest.mark.xfail(strict=True)
-def test_delete_symbol_negative_two():
+def test_delete_symbol_n2():
     string_utils = StringUtils()
-    res = string_utils.delete_symbol("Anton", "A")
+    res = string_utils.delete_symbol("Anton", 1)
     assert res == "Anton"
 
 
@@ -87,12 +101,23 @@ def test_starts_with():
     string_utils = StringUtils()
     res = string_utils.starts_with("Anton", "A")
     assert res == True
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
-def test_starts_with_negative():
+def test_starts_with_n1():
     string_utils = StringUtils()
     res = string_utils.starts_with("Anton", "n")
     assert res == True
-
+@pytest.mark.xfail(strict=True)
+def test_starts_with_n2():
+    string_utils = StringUtils()
+    res = string_utils.starts_with("Anton", "")
+    assert res == True
+@pytest.mark.xfail(strict=True)
+def test_starts_with_n3():
+    string_utils = StringUtils()
+    res = string_utils.starts_with("Anton", 1)
+    assert res == True
 
 
 ## Проверка буквы окончания слова
@@ -100,10 +125,22 @@ def test_end_with():
     string_utils = StringUtils()
     res = string_utils.end_with("Anton", "n")
     assert res == True
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
-def test_end_with_negative():
+def test_end_with_n1():
     string_utils = StringUtils()
     res = string_utils.end_with("Anton", "A")
+    assert res == True
+@pytest.mark.xfail(strict=True)
+def test_end_with_n2():
+    string_utils = StringUtils()
+    res = string_utils.end_with("Anton", " ")
+    assert res == True
+@pytest.mark.xfail(strict=True)
+def test_end_with_n3():
+    string_utils = StringUtils()
+    res = string_utils.end_with("Anton", 123)
     assert res == True
 
 ## Проверка пустой строки
@@ -115,13 +152,15 @@ def test_is_empty_two():
     string_utils = StringUtils()
     res = string_utils.is_empty(" ")
     assert res == True
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
-def test_is_empty_negative():
+def test_is_empty_n1():
     string_utils = StringUtils()
     res = string_utils.is_empty(123)
-    assert res == False
+    assert res == True
 @pytest.mark.xfail(strict=True)
-def test_is_empty_negative_two():
+def test_is_empty_n2():
     string_utils = StringUtils()
     res = string_utils.is_empty("Антон")
     assert res == True
@@ -131,18 +170,20 @@ def test_list_to_string():
     string_utils = StringUtils()
     res = string_utils.list_to_string(["A","n","t","o","n"])
     assert res == ("A, n, t, o, n")
+
+## Негативные проверки
 @pytest.mark.xfail(strict=True)
-def test_list_to_string_negative():
+def test_list_to_string_n1():
     string_utils = StringUtils()
-    res = string_utils.list_to_string(["A","n","t","o","n"])
-    assert res == ["A, n, t, o, n"]
+    res = string_utils.list_to_string([123])
+    assert res == [123]
 @pytest.mark.xfail(strict=True)
-def test_list_to_string_negative_two():
+def test_list_to_string_n2():
     string_utils = StringUtils()
-    res = string_utils.list_to_string(["A","n","t","o","n"])
+    res = string_utils.list_to_string([" "])
     assert res == []
 @pytest.mark.xfail(strict=True)
-def test_list_to_string_negative_three():
+def test_list_to_string_n3():
     string_utils = StringUtils()
-    res = string_utils.list_to_string(["A","n","t","o","n"])
-    assert res == ("")
+    res = string_utils.list_to_string(("123"))
+    assert res == ("123")
