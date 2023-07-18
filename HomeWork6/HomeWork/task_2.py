a4c48604f36bdd6d038e042c137559fb1fbd0555
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-18 21:39:22 +0300

    save

--- HomeWork6/HomeWork/task_2.py
+++ HomeWork6/HomeWork/task_2.py
@@ -4,11 +4,11 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("http://uitestingplayground.com/textinput")
-
-sleep(5)
 
 input_in = driver.find_element(By.CSS_SELECTOR, "input").click()
 
@@ -24,4 +24,6 @@ driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
 txt = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
 print('Кнопка после изменения ' + txt)
 
-sleep(5)
+waiter = WebDriverWait(driver, 15, 5).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"#updatingButton"), "SkyPro"))
+
+driver.quit()
