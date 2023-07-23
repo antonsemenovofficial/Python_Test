bf30d30d27796645ed16d682cbeeacd65d010c51
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-23 14:49:28 +0300

    save

--- HomeWork7/Lessons/labirint_test.py
+++ HomeWork7/Lessons/labirint_test.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 cookie = {
     "name": "cookie_policy",
@@ -24,19 +26,28 @@ def test_cart_counter():
     browser.find_element(By.CSS_SELECTOR, "#search-field").send_keys("Python")
     browser.find_element(By.CSS_SELECTOR,"button[type=submit]").click()
     
+    sleep(4)
+
     ##переключиться на таблицу
     browser.find_element(By.CSS_SELECTOR,"a[title='таблицей']").click()
 
+    WebDriverWait(browser, 20).until(
+     EC.presence_of_element_located((By.CSS_SELECTOR, "table"))
+    )
+
     ##добавить все книги в коррзину и посчитать, сколько
     buy_buttons = browser.find_elements(By.CSS_SELECTOR, ".btn.buy-link.btn-primary")
+    counter = 0
     for btn in buy_buttons:
         btn.click()
+        counter += 1
 
     ##перейти в корзину
-
+    browser.get("https://www.labirint.ru/cart/")
     ##поверить счетчик товаров
+    txt = browser.find_element(By.CSS_SELECTOR,"a[data-event-label='myCart']").find_element(By.CSS_SELECTOR, "b").text
 
-    sleep(5)
+    assert counter == int(txt)
 
 
     browser.quit()
