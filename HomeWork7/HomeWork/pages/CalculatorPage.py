c0119fa434fe83b4e2830b1518c68a56c2ae8f13
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-07-23 17:54:49 +0300

    well done

---
+++ HomeWork7/HomeWork/pages/CalculatorPage.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.MainPageTask1 import MainPageTask1
+
+
+class CalculatorPage:
+
+    def __init__(self, browser):
+        self.driver = browser
+
+    def get(self):
+        self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    def first_number(self):
+        key = self.driver.find_element(By.CSS_SELECTOR, "div.keys > span:nth-child(1)").click()
+   
+    def operator(self):
+        key = self.driver.find_element(By.CSS_SELECTOR, ".operator.btn.btn-outline-success").click()
+
+    def second_number(self):
+        key = self.driver.find_element(By.CSS_SELECTOR, "div.keys > span:nth-child(2)").click()
+    
+    def ravno(self):
+        self.driver.find_element(By.CSS_SELECTOR, ".btn.btn-outline-warning").click()
+
+    def result(self):
+        results = '15'
+        WebDriverWait(self.driver, 70).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"),results)
+        )
+       
---
+++ HomeWork7/HomeWork/task2.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.MainPageTask1 import MainPageTask1
+from pages.CalculatorPage import CalculatorPage
+
+def test_calculator():
+    browser = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+
+    main_page = MainPageTask1(browser)
+    main_page.get()
+    main_page.set_seconds('45')
+
+    calculator_page = CalculatorPage(browser)
+    calculator_page.first_number()
+    calculator_page.operator()
+    calculator_page.second_number()
+    calculator_page.ravno()
+
+    calculator_page.result()
