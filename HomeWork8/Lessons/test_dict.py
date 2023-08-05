a3042cca17a27bff846598e6f5f70f245a0859f8
antonsemenovofficial <antonsemenovofficial@yandex.ru>
2023-08-05 07:45:00 +0300

    save

--- HomeWork8/Lessons/test_dict.py
+++ HomeWork8/Lessons/test_dict.py
@@ -1,5 +1,16 @@
-empty_dict = {}
+import json
 
-def test_empty_dict():
-    value = empty_dict["key"]
-    assert value == None
+company_json = """ {
+"id" : 111,
+"isActive" : true,
+"createDateTime" : "2022-12-23T14:43:26.713Z",
+"lastChangedDateTime" : "2022-12-23T14:43:26.713Z",
+"name" : "Барбершоп Цирюьник",
+"description" : "Крутые стрижки для кртух шишек"
+
+}
+"""
+
+def test_parse_json():
+  company = json.loads(company_json)
+  assert company["id"]==111
